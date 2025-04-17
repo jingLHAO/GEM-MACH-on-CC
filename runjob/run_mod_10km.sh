d9abb0dadd9d02894224b69cd6a4f8d16c5b00a2
jingLHAO <hao11@yorku.ca>
2025-04-17 13:46:33 -0400

    Add files via upload

---
+++ runjob/run_mod_10km.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script submits GEM-MACH on cedar cluster (Compute Canada)
+# ***** change to your own username and mail address *****
+
+#SBATCH --time=00-03:00:00         # time (DD-HH:MM:SS)
+#SBATCH --job-name="run_mod"     # job name
+
+#SBATCH --cpus-per-task=1                # cpu per task
+#SBATCH --ntasks=1702
+#SBATCH --mem-per-cpu=3G             # memory per cpu
+#SBATCH --account=def-yorkaqrl
+#SBATCH --mail-user=abc123456@gmail.com   # send an email to a specific address
+#SBATCH --mail-type=BEGIN           # send an email when the job begins
+#SBATCH --mail-type=FAIL          # send an email when the job fails
+#SBATCH --mail-type=END          # send an email when the job ends
+#SBATCH --output=username-%j.log                 # log file
+#SBATCH --error=username-%j.err                  # error file
+
+cd  $MY_DEFAULT_PATH
+set | egrep SLURM  | sed -e 's/^/export /g' > reset_slurm_env
+echo "slurm_hl2hl.py   --format MPIHOSTLIST > $SLURM_SUBMIT_DIR/hostfile" >> reset_slurm_env
+echo "alias mpiexec='mpiexec --hostfile $SLURM_SUBMIT_DIR/hostfile ' " >> reset_slurm_env
+echo "alias mpirun='mpirun --hostfile $SLURM_SUBMIT_DIR/hostfile ' " >> reset_slurm_env
+source ./reset_slurm_env
+module load StdEnv/2023 intel/2023.2.1 imkl/2023.2.0 openmpi/4.1.5  fftw/3.3.10
+. ./.common_setup intel
+. /$MY_DEFAULT_PATH/scripts/runmod.sh -dircfg $MY_DEFAULT_PATH/work-AlmaLinux-9.3-x86_64-intel-2021.10.0/configurations/os_v524_machOSrun5 -ptopo 37x46x1 -inorder
